02214acd3f24af499209ee78b8816993a797e8fc
Nils Streijffert <nils.streijffert@gmail.com>
2020-02-03 14:01:30 +0100

    feat/#2 Add the skeleton code from Canvas with some changes. Also added some helper functions to parse the JSON-payload in the GitHub POST request. fixes #2 (#7)

---
+++ src/main/java/server/ContinuousIntegrationServer.java
@@ -0,0 +1,42 @@
+package server;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ContinuousIntegrationServer extends AbstractHandler {
+    private static final Logger logger = Logger.getLogger(ContinuousIntegrationServer.class);
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new ContinuousIntegrationServer());
+        server.start();
+        server.join();
+    }
+
+    @Override
+    public void handle(String target, @NotNull Request baseRequest, @NotNull HttpServletRequest request, @NotNull HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        if ("POST".equalsIgnoreCase(request.getMethod())) {
+            String payload = IOUtils.toString(request.getReader());
+            try {
+                JSONObject jsonObject = new JSONObject(payload);
+            } catch (JSONException e) {
+                logger.error("Server failed while parsing received payload", e);
+            }
+        }
+        response.getWriter().println("CI job running");
+    }
+}
