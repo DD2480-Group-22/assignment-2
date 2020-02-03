a2730e8674aef4c3201e807f907bdb1e7e3bd57b
Nils Streijffert <nils.streijffert@gmail.com>
2020-02-03 14:14:14 +0100

    feat/#8 Add the functionality to clone the Github repository specified by the GitHub POST request. fixes #8

---
+++ src/main/java/ci/GitRepositoryHandler.java
@@ -0,0 +1,58 @@
+package ci;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import utilities.Configuration;
+
+import java.io.File;
+
+public class GitRepositoryHandler {
+    private static final Logger logger = Logger.getLogger(GitRepositoryHandler.class);
+    private final String id;
+    private final String repositoryName;
+    private final String cloneURL;
+
+    /**
+     * Creates a GitRepositoryHandler object
+     *
+     * @param id             the build id for the project
+     * @param repositoryName the name of the repository
+     * @param cloneURL       the URL used for cloning the repository
+     */
+    public GitRepositoryHandler(final String id, final String repositoryName, final String cloneURL) {
+        this.id = id;
+        this.repositoryName = repositoryName;
+        this.cloneURL = cloneURL;
+    }
+
+    /**
+     * Clones specified github repository
+     *
+     * @return {@code true} if the repository was successfully cloned, otherwise {@code false}
+     */
+    public boolean cloneRepository() {
+        logger.info("Cloning repository: " + repositoryName);
+
+        CloneCommand cloneCommand = Git.cloneRepository();
+        cloneCommand.setURI(cloneURL);
+        cloneCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider(Configuration.GITHUB_TOKEN, ""));
+        cloneCommand.setDirectory(new File(Configuration.PATH_TO_GIT + id + "/" + repositoryName));
+        try {
+            cloneCommand.call();
+            return true;
+        } catch (TransportException e) {
+            logger.error("Transport operation from Github remote failed", e);
+        } catch (InvalidRemoteException e) {
+            logger.error("Invalid remote repository", e);
+        } catch (GitAPIException e) {
+            logger.error("Something went wrong with the Github API", e);
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/utilities/Configuration.java
@@ -0,0 +1,7 @@
+package utilities;
+
+public class Configuration {
+    public static final String GITHUB_TOKEN = System.getenv("GITHUB_TOKEN");
+    public static final String PATH_TO_RESOURCES = "./";
+    public static final String PATH_TO_GIT = PATH_TO_RESOURCES + "git/";
+}
