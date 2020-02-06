fc86c01abb7fa805b3dc1e50a737ef2aa7d715a4
Angmar <erik.r.svensson@telia.com>
2020-02-06 16:04:36 +0100

    feat/#47 Code skeleton
    Add a code skeleton for the class that will be used to test the functions in Helpers.

---
+++ src/test/java/org/group22/utilities/HelpersTest.java
@@ -0,0 +1,22 @@
+package org.group22.utilities;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.geom.Point2D;
+
+public class HelpersTest {
+	@Nested
+	@DisplayName("Tests the generate ID function")
+	class GenerateIDTests {
+		@Test
+		@DisplayName("Basic test")
+		void basicTest() {
+			
+		}
+
+	}
+}
