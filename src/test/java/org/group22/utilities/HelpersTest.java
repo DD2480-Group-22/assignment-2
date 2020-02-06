582973859fcdc8056d0043ea586126e8520c66ee
felipevicencion <60047540+felipevicencion@users.noreply.github.com>
2020-02-06 20:25:41 +0100

    fix/#47 fixed maven plugin so it can run tests

--- src/test/java/org/group22/utilities/HelpersTest.java
+++ src/test/java/org/group22/utilities/HelpersTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.awt.geom.Point2D;
@@ -15,7 +18,9 @@ public class HelpersTest {
 		@Test
 		@DisplayName("Basic test")
 		void basicTest() {
-			
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+        	LocalDateTime now = LocalDateTime.now();
+        	assertEquals("id_" + dtf.format(now), Helpers.generateId("id"));
 		}
 
 	}
